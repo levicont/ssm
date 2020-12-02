d54048da8d99288ad64d70ba6fdbb58690514b55
levicont <victor84lg@mail.ru>
2020-12-02 13:02:33 +0200

    Fixed some issues in Filler

--- src/main/java/com/lvg/ssm/Filler.java
+++ src/main/java/com/lvg/ssm/Filler.java
@@ -43,23 +43,23 @@ public class Filler {
             System.out.println("Date: "+ formatDate(testReport.getDate()));
             setCellTextByPosition(xSpreadsheet,5,9,testReport.getNumber());
             setCellTextByPosition(xSpreadsheet,7,9,formatDate(testReport.getDate()));
-            xSpreadsheet.getCellByPosition(3, 13).setFormula(testReport.getWorkingDrawings());
-            xSpreadsheet.getCellByPosition(8, 17).setFormula(testReport.getWelder());
-            xSpreadsheet.getCellByPosition(8, 18).setFormula(testReport.getWelderEng());
-            xSpreadsheet.getCellByPosition(8, 20).setFormula(testReport.getWelderMark());
-            xSpreadsheet.getCellByPosition(8, 21).setFormula(testReport.getWelderMarkEng());
+            setCellTextByPosition(xSpreadsheet,3, 13, testReport.getWorkingDrawings());
+            setCellTextByPosition(xSpreadsheet,8, 17, testReport.getWelder());
+            setCellTextByPosition(xSpreadsheet,8, 18, testReport.getWelderEng());
+            setCellTextByPosition(xSpreadsheet,8, 20, testReport.getWelderMark());
+            setCellTextByPosition(xSpreadsheet,8, 21, testReport.getWelderMarkEng());
 
             int appendixStartRow = 76;
             for(int i = 0; i < testReport.getAppendixEntities().size(); i++ ){
                 TestReport.AppendixEntity appendixEntity = testReport.getAppendixEntities().get(i);
                 try {
-                    xSpreadsheet.getCellByPosition(0,appendixStartRow).setFormula((i+1)+"");
-                    xSpreadsheet.getCellByPosition(1,appendixStartRow).setFormula(appendixEntity.getWorkingDrawings());
-                    xSpreadsheet.getCellByPosition(3,appendixStartRow).setFormula(appendixEntity.getPosition());
-                    xSpreadsheet.getCellByPosition(4,appendixStartRow).setFormula(appendixEntity.getPositionName());
-                    xSpreadsheet.getCellByPosition(6,appendixStartRow).setFormula(appendixEntity.getAmount()+"");
-                    xSpreadsheet.getCellByPosition(7,appendixStartRow).setFormula(appendixEntity.getCondition().toString());
-                    xSpreadsheet.getCellByPosition(8,appendixStartRow).setFormula(appendixEntity.getComments());
+                    setCellTextByPosition(xSpreadsheet,0, appendixStartRow, (i+1)+"");
+                    setCellTextByPosition(xSpreadsheet,1, appendixStartRow, appendixEntity.getWorkingDrawings());
+                    setCellTextByPosition(xSpreadsheet,3, appendixStartRow, appendixEntity.getPosition());
+                    setCellTextByPosition(xSpreadsheet,4, appendixStartRow, appendixEntity.getPositionName());
+                    setCellTextByPosition(xSpreadsheet,6, appendixStartRow, appendixEntity.getAmount()+"");
+                    setCellTextByPosition(xSpreadsheet,7, appendixStartRow, appendixEntity.getCondition().toString());
+                    setCellTextByPosition(xSpreadsheet,8, appendixStartRow, appendixEntity.getComments());
                     appendixStartRow++;
                 }catch (Exception ex){
                     throw new RuntimeException(ex);
